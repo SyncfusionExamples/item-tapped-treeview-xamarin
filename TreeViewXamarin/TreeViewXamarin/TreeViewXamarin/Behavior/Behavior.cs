b5bfbe471d12cfd720278a405391486e45f91b19
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-08-26 17:49:38 +0530

    XAMARIN-35884 - TreeView sample ItemTapped event added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Behavior/Behavior.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfTreeView treeView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            treeView = bindable.FindByName<SfTreeView>("treeView");
+            treeView.ItemTapped += TreeView_Itemapped;
+        }
+
+        private void TreeView_Itemapped(object sender, Syncfusion.XForms.TreeView.ItemTappedEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("Item Tapped", "TreeView item tapped", "Close");
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            treeView.ItemTapped -= TreeView_Itemapped;
+        }
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.XForms.TreeView;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
